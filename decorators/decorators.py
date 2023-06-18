95411bff2bba5763ff6ad48a7c4791535a71c703
Vikli <hnat.ilkiv.ir.2022@lpnu.ua>
2023-06-18 09:10:09 +0300

    fixed decorators @wraps

--- decorators/decorators.py
+++ decorators/decorators.py
@@ -1,8 +1,11 @@
 import logging
+from functools import wraps
 from exceptions.exceptions import NoSuchModeException
 
 def logged(exception, mode="console"):
+
     def decorator(func):
+        @wraps(func)
         def wrapper(*args, **kwargs):
             try:
                 result = func(*args, **kwargs)
@@ -11,7 +14,9 @@ def logged(exception, mode="console"):
                 if mode == "console":
                     logging.error(ex.log_message)
                 elif mode == "file":
-                    logging.basicConfig(filename='log.txt', level=logging.ERROR)
+                    logging.basicConfig(filename='log.txt',
+                                        filemode="a", # w - overwrite | a - add
+                                        level=logging.ERROR)
                     logging.error(ex.log_message)
                 else:
                     raise NoSuchModeException() from ex
